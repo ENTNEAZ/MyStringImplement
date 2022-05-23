f08b0f8bfdc95f7f23aa139132859a9e04e7137c
ENTNEAZ <langyuez@outlook.com>
2022-05-23 22:32:34 +0800

    添加项目文件。

---
+++ MyStringImplement/MyString.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef MyString
+
+class MyString {
+public:
+	static void* memcpy(void* destination, const void* source, size_t num);
+};
+
+#endif // !MyString
---
+++ MyStringImplement/StaticMethod.cpp
@@ -0,0 +1,26 @@
+#include "MyString.h"
+
+void* MyString::memcpy(void* destination, const void* source, size_t num)
+{
+	if ((char*)source + num < destination || (char*)destination + num < source) {
+		//两个区域没有重叠 直接覆盖
+		const char* s = (char*)source;
+		char* d = (char*)destination;
+		while (num--) {
+			*d = *s;
+			d++;
+			s++;
+		}
+	}
+	else {
+		//要写入的部分有重叠
+		//需要倒着写入
+		char* d = (char*)((char*)destination + num - 1); /* offset of pointer is from 0 */
+		const char* s = (char*)((char*)source + num - 1);
+		while (num--){
+			*d-- = *s--;
+		}
+
+	}
+	return destination;
+}
